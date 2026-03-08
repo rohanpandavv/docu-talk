ec5a977db14b98ec1d8cd4b59353c23b8ac5a9a6
rohanpandavv <rohanpandav9@gmail.com>
2026-03-08 21:38:40 +0530

    update: main.py
    add upload function with validations

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, HTTPException
 from pydantic import BaseModel
 from langchain_community.vectorstores import Chroma
 from langchain_anthropic import ChatAnthropic
 from langchain_community.embeddings import SentenceTransformerEmbeddings
 from dotenv import load_dotenv
 from pathlib import Path
+from pypdf import PdfReader
+from io import BytesIO
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 BASE_DIR = Path(__file__).resolve().parent
 load_dotenv(BASE_DIR/".env")
@@ -18,3 +21,24 @@ llm = ChatAnthropic(model="claude-haiku-4-5-20251001")
 class ChatRequest(BaseModel):
     question: str
 
+@app.post("/upload")
+async def upload_document(file: UploadFile = File(...)):
+    content = await file.read()
+    print("content_type ----> ", file.content_type)
+    content_type = file.content_type.lower()
+    if content_type == "application/pdf":
+        pdf =  PdfReader(BytesIO(content))
+        text = "\n".join(page.extract_text() or "" for page in pdf.pages)
+    elif content_type == "text/plain":
+        try:
+            text = content.decode("utf-8")
+        except UnicodeDecodeError:
+            raise HTTPException(status_code=400, detail="Text file is not valid UTF-8")
+    else:
+        raise HTTPException(status_code=400, detail="Unsupported file type.")
+    
+    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    chunks = splitter.split_text(text)
+    
+    vectorstore.add_texts(texts=chunks)
+    return {"message": "Document indexed successfully!"}
